755338ef42f3a4f6b7f52535f6c8e9aad1888f77
timepass5738 <timepass5738@gmail.com>
2024-10-24 03:26:01 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,5 +3,5 @@ from time import sleep
 camera = PiCamera()
 camera.start_preview() #start camera
 sleep(5)
-camera.capture(&#39;/home/pi/Desktop/image.jpg&#39;) # take a picture
+camera.capture('/home/pi/Desktop/image.jpg') # take a picture
 camera.stop_preview()
